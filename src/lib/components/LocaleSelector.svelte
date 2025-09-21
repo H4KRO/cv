524a6062de038f5eb84c4be40ce07e75330dfcab
Eddie Vallier <32486404+H4KRO@users.noreply.github.com>
2025-09-21 21:33:41 +0200

    Save

--- src/lib/components/LocaleSelector.svelte
+++ src/lib/components/LocaleSelector.svelte
@@ -1,22 +1,21 @@
 <script lang="ts">
 	import { setLocale } from '$lib/paraglide/runtime';
-	export let availableLocales: string[] = [];
+	export let availableLocales: string[] = ['en', 'fr'];
 	export let currentLocale: string;
 	let isOpen = false;
 
-	function toggle() {
-		isOpen = !isOpen;
-	}
-
+	function toggle() { isOpen = !isOpen; }
 	function selectLocale(locale: string) {
 		setLocale(locale);
 		currentLocale = locale;
 		isOpen = false;
 	}
+	function handleBlur() { setTimeout(() => { isOpen = false; }, 200); }
 
-	function handleBlur() {
-		setTimeout(() => { isOpen = false; }, 200);
-	}
+	const localeFlags = {
+		en: 'us',
+		fr: 'fr'
+	};
 </script>
 
 <div class="relative inline-block">
@@ -27,25 +26,23 @@
 		aria-haspopup="listbox"
 		aria-expanded={isOpen}
 	>
-		{currentLocale}
+		<span class="fi fi-{localeFlags[currentLocale]} cursor-pointer"></span>
 	</button>
 
 	{#if isOpen}
-		<ul
-			class="absolute left-0 md:bottom-auto md:top-full top-auto bottom-full"
-			role="listbox"
-			tabindex="-1"
-		>
+		<ul class="absolute left-0 md:bottom-auto md:top-full top-auto bottom-full" role="listbox" tabindex="-1">
 			{#each availableLocales as locale (locale)}
+				{#if locale != currentLocale}
 				<li>
 					<button
 						type="button"
 						on:click={() => selectLocale(locale)}
 						role="option"
 					>
-						{locale}
+						<span class="fi fi-{localeFlags[locale]} cursor-pointer"></span>
 					</button>
 				</li>
+					{/if}
 			{/each}
 		</ul>
 	{/if}
